bfee20f9a6ed1add074cdecab65dab97ac18e226
SUZIN <suuz.hha@gmail.com>
2021-03-02 11:39:08 +0900

    add spring study

---
+++ JSL/spring/exam01/src/main/java/com/jslhrd/domain/BoardVO.java
@@ -0,0 +1,17 @@
+package com.jslhrd.domain;
+
+public class BoardVO {
+	private int idx;
+	private String name;
+	private String email;
+	private String pass;
+	private String subject;
+	private String contents;
+	private String regdate;
+	private int readcnt;
+	
+	
+	
+	
+	
+}
